0118dfb63caed0195a199407ce234d2ea5b7bdf5
Sean Ng <sean.ng.cip@gmail.com>
2023-04-17 14:37:19 -0700

    Refactor layout and edit project descriptions

--- src/components/organisms/Artifact.tsx
+++ src/components/organisms/Artifact.tsx
@@ -16,20 +16,20 @@ export default function ProjectPage({ artifactName, description, tags, links }: 
 
   return (
     <>
-      <div className="flex flex-col pl-4 pr-36 pt-24 pb-16">
-        <div className="grid grid-cols-2 mx-6 md:mx-16 text-left">
+      <div className="flex flex-col pt-24 pb-16">
+        <div className="grid grid-cols-2 text-left">
           <div className="px-4 pt-6">
-            <div className="flex flex-row mx-32">              
+            <div className="flex flex-row mx-16">              
               <TitleText text={artifactName} />
             </div>
-            <div className="align-center pt-8 pb-6 pl-8 pr-8 ml-20">
+            <div className="align-center pt-8 pb-6 px-8">
               <DescriptionBox description={description} tags={tags} links={links}/>
             </div>
           </div>
           <div className="">
             <div className="relative">
               <a href={links[1]} rel="noopener noreferrer" target="_blank">
-                <Image src={`/images/${artifactName.toLowerCase()}.png`} alt={`${artifactName} Homepage`} width={1000} height={1000} className={"mt-6 rounded-lg hover:scale-105 duration-300"} />
+                <Image src={`/images/${artifactName.toLowerCase()}.png`} alt={`${artifactName} Homepage`} width={800} height={800} className={"mt-12 pr-12 rounded-lg hover:scale-105 duration-300"} />
               </a>
             </div>
           </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,19 +12,19 @@ export default function Index() {
   const projects = [
     {
       name: "Buddytree",
-      description: "Buddytree is a social media app I helped maintain with a team of developers as part of BCIT's co-op program. Buddytree helps users discover and meet new trusted individuals and friends. Users can create and join groups, see nearby users, and create hangouts for other users to attend.",
+      description: "Buddytree is a social media app I helped maintain as part of BCIT's co-op program. Buddytree helps users discover and meet new trusted individuals and friends. Users can create and join groups, see nearby users, and create hangouts for other users to attend.",
       tags: ["React", "Next.js", "TypeScript", "SASS", "Flask", "Python", "PostgresQL", "Heroku"],
       links: ["", "https://www.buddytree.org/"]
     },
     {
       name: "InterviewPal",
-      description: `Voted as the #1 Audience Choice Winner at the BCIT's HackTheBreak 2023, InterviewPal is a GPT-3.5 powered AI tool that helps you practice for your interviews. It asks you common behavioral, situational, and technical interview questions and provides insightful feedback about your responses.`,
+      description: `Voted the #1 Audience Choice Winner at the BCIT's HackTheBreak 2023, InterviewPal is a GPT-3.5 powered AI tool that helps you practice for your interviews. It asks you common behavioral, situational, and technical interview questions and provides insightful feedback about your responses.`,
       tags: ["React", "Next.js", "Typescript", "Tailwind", "Redis", "OpenAI", "Vercel"],
       links: ["https://github.com/InterviewPal/InterviewPal", "https://interviewpal.vercel.app/"]
     },
     {
       name: "IGAQ",
-      description: "I Got A Queery, or IGAQ, is an internet forum that provides a safe, online space for members and allies of the LGBTQ+ community to ask questions, share stories, and read resources about all things LGBTQ+, built for BCIT's Interdisciplinary Project program in a team with six other developers and designers.",
+      description: "I Got A Queery, or IGAQ, is an internet forum that provides a safe, online space for members and allies of the LGBTQ+ community to ask questions, share stories, and read resources about all things LGBTQ+, built for BCIT's Interdisciplinary Projects with a team of designers and other developers.",
       tags: ["React", "Next.js", "JavaScript", "TypeScript", "NestJS", "Neo4j", "Vercel"],
       links: ["https://github.com/IGAQ/NextApp", "https://www.igaq.gay/"]
     },
@@ -35,6 +35,7 @@ export default function Index() {
       links: ["https://github.com/iantelli/word-gotchu", "https://word-gotchu.up.railway.app/"]
     },
   ]
+
   return (
     <>
       <Head>
@@ -49,28 +50,23 @@ export default function Index() {
         <div id="" className="fixed h-screen z-0">
           <video src={"./files/background.webm"} autoPlay={true} loop muted className="object-fill w-screen h-screen pointer-events-none"></video>
         </div>
-        <div className="relative z-10">
-          <div className="flex flex-col justify-start ml-60 px-24 py-24 xl:py-64 h-screen w-3/5 drop-shadow-lg">
+        <div className="relative z-10 w-4/5 m-auto">
+          <div className="flex flex-col justify-start pl-32 py-24 xl:py-64 h-screen drop-shadow-lg">
             <HeroText text={"Hi, I'm Sean Ng"} spacing="pt-16" />
             <TitleText text={"Full Stack Developer"} spacing="py-4" />
-            <HomepageText text={"I'm a full stack developer based in Vancouver, Canada, specializing in building intuitive and robust web applications."} />
+            <HomepageText text={"I'm a full stack developer based in Vancouver, Canada,"} />
+            <HomepageText text={"specializing in building intuitive and robust web applications."} />
             <ContactLinks decoration="mt-2 -ml-2" />
           </div>
-        </div>
-        <div className="relative z-10">
           <div id="projects" className="flex flex-col items-center justify-center pt-36 mb-8 drop-shadow-lg">
             <TitleText text={"Projects I Worked On"} spacing="p-8" />
             {projects.map((project) => (
               <Artifact key={project.name} artifactName={project.name} description={project.description} tags={project.tags} links={project.links} />
               ))}          
           </div>
-        </div>
-        <div className="relative z-10">
           <div id="contact" className="flex flex-col items-center justify-center pt-48 pb-36 drop-shadow-lg">
             <ContactBox />
           </div>
-        </div>
-        <div className="relative z-10">
           <span className="flex justify-center text-sm text-white drop-shadow-lg pb-4 hover:opacity-50 duration-300">
             <a href="https://wallpaperwaifu.com/">Background From WallpaperWaifu</a>
           </span>
